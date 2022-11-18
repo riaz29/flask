f8ff168bd45c7e61a7afb76577e1757102d21c53
unknown <sh.riaz29@gmail.com>
2022-11-18 21:31:07 +0500

    Adding Templates

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html', name="riaz")
+
+@app.route('/about')
+def about():
+    return render_template('about.html',about = "We are IT Professional")
 
 if __name__ == '__main__':
    app.run()
